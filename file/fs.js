2533adaefbb89004c45de856e333474802811841
hittdx <940165364@qq.com>
2017-08-18 14:44:07 +0800

    1.

---
+++ file/fs.js
@@ -0,0 +1,10 @@
+let fs = require('fs');
+
+fs.readFile('input.txt',function(err,data){
+    if (err){
+        return console.error(err);
+    }
+    console.log('异步读取：' + data);
+})
+
+console.log('finished!');
